463b8291be87ea986840f5b27b9b6bc118adbb4c
Fredrik000 <52358347+Fredrik000@users.noreply.github.com>
2023-01-30 17:58:59 +0100

    desktop version finished

--- src/components/FactsCard.tsx
+++ src/components/FactsCard.tsx
@@ -46,11 +46,27 @@ const FactsCard: Component<IPlanet> = ({
         <h1 class='uppercase'>{name}</h1>
         <p>{content}</p>
 
-        <div class='flex items-center'>
-          <p class='opacity-50'>Source: </p>
-          <h4 class='opacity-50'>
-            <a href={source()}>Wikipedia</a>
-          </h4>
+        <div class='flex items-center gap-2'>
+          <p class='opacity-50'>Source:</p>
+          <p class='opacity-50 flex'>
+            <a href={source()} class='font-bold underline flex gap-1'>
+              Wikipedia
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke-width='1.5'
+                stroke='currentColor'
+                class='w-4 h-4 text-white'
+              >
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244'
+                />
+              </svg>
+            </a>
+          </p>
         </div>
 
         <div class='flex flex-col gap-4'>
